a96b54334e679d815b200a73bdfd3938a7b9169b
kejiblueprints <kejiblueprints@gmail.com>
2022-12-10 05:17:49 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,12 @@
 main(void)
 int main(void)
 {
-prinf(Size of a char : % i byte(s)n", sizeof(char));
-printf(Size of an int: %i byte(s)\n", sizeof(int));
-printf(Size of a long int: % i byte(s)\n", sizeof(long int));
-printf("Size of a long long int: % i byte(s)\n", sizeof(long long int));
-printf(Size of a float: %i byte(s)\n", sizeof(float));
-printf(Size of a long long int: % i byte(s)\n", sizeof(long long int));
-printf(Size of a float: %i byte(s)\n", sieof(float));
+prinf(Size of a char : % i byte(s)n" sizeof(char));
+printf(Size of an int: %i byte(s)\n" sizeof(int));
+printf(Size of a long int: % i byte(s)\n" sizeof(long int));
+printf("Size of a long long int: % i byte(s)\n" sizeof(long long int));
+printf(Size of a float: %i byte(s)\n" sizeof(float));
+printf(Size of a long long int: % i byte(s)\n" sizeof(long long int));
+printf(Size of a float: %i byte(s)\n" sieof(float));
 return (0);
 }
