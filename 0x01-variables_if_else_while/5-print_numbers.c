c02808ef8288f73b1237cfcc6e15bf1c2b3bf7a5
kejiblueprints <kejiblueprints@gmail.com>
2022-12-12 03:29:43 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main-it prints numbers from 0 to 9
+*
+*Return: 0 if successful
+*/
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
