311f38f8cd2123442736027019178467576ef819
kejiblueprints <kejiblueprints@gmail.com>
2022-12-16 09:41:09 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 /**
 *print_alphabet- prints the alphabet a-z to the screen
 */
 void print_alphabet(void)
 {
-int a;
+	int a = 'a';
 
-for (a = 'a'; a <= 'z'; a++);
-{
-_putchar(a);
-}
-_putchar('\n');
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
 }
